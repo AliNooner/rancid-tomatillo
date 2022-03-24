af6a0f9e8f965c2d229612a65fc137625f26c1bd
Ali Nooner <alinooner@gmail.com>
2022-03-24 16:11:11 -0500

    Create MovieCard instance

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,25 @@
-// import React from "react";
-// import MovieCard from "./MovieCard";
-// import "./Movies.css";
-//
-// const Movies = () => {
-//   return (
-//
-//   )
-// }
-//
-// export default Movies;
+import React from "react";
+import MovieCard from "./MovieCard";
+import "./Movies.css";
+
+const Movies = ({movieData}) => {
+
+  const movieCards = movieData.map(movie => {
+    return (
+      <MovieCard
+        title = {movie.title}
+        poster = {movie.poster_path}
+        id = {movie.id}
+        key = {movie.id}
+      />
+    )
+  })
+
+  return (
+    <div className="movie-container">
+      {movieCards}
+      </div>
+  )
+}
+
+export default Movies;
