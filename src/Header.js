f13e8482713497a03c312261d7a069ad9cc45f5a
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-03-23 22:27:54 -0600

    Add Header basic function and css file

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Header;
