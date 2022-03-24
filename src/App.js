87ae602a6bd660f0016edfda7696aaa1c0700270
Ali Nooner <alinooner@gmail.com>
2022-03-24 15:57:50 -0500

    Add data to state in App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Header";
 import Movies from "./Movies";
+import movieData from './data';
 
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-
+      movieData
     }
   }
 
@@ -19,22 +20,6 @@ class App extends React.Component {
         <h1>Rancid Tomatillos</h1>
         <Header />
       </main>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>
-      //       Edit <code>src/App.js</code> and save to reload.
-      //     </p>
-      //     <a
-      //       className="App-link"
-      //       href="https://reactjs.org"
-      //       target="_blank"
-      //       rel="noopener noreferrer"
-      //     >
-      //       Learn React
-      //     </a>
-      //   </header>
-      // </div>
     );
   }
 }
