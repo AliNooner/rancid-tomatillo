edbdd4ccf1fec5d914b9e316ec13c71265de351a
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-03-26 12:02:22 -0600

    Refactor css

--- src/SingleMovieCard.js
+++ src/SingleMovieCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./SingleMovieCard.css";
-import MovieCard from "./MovieCard";
-import Movies from "./Movies";
+// import MovieCard from "./MovieCard";
+// import Movies from "./Movies";
 
 const SingleMovieCard = ({ singleMovie, displayMovieInfo}) => {
   return (
     <div className="single-movie-card">
+      <button className="x-button">X</button>
       <h1>{singleMovie.title}</h1>
       <img className="backdrop-poster-img" src={singleMovie.backdrop_path} alt={singleMovie.title}/>
       <h3>Release Date: {singleMovie.release_date}</h3>
