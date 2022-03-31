aeae9c7929e18e5ad35a94787e2412bc5d7bd1c0
Ali Nooner <alinooner@gmail.com>
2022-03-30 20:08:56 -0500

    Add testing for single movie page

--- cypress/integration/moviesPage.js
+++ cypress/integration/moviesPage.js
@@ -1,20 +1,19 @@
-
 // true testing
 describe("Load", () => {
   it("Should confirm that true is equal to true", () => {
     expect(true).to.equal(true);
   });
-// header title
+  // header title
   it("Should have a header with text Rancid Tomatillos on load", () => {
     cy.visit("http://localhost:3000/").contains("Rancid Tomatillos");
   });
-//form
+  //form
   it("Should contain a search bar", () => {
     cy.get('form input[placeholder="Search title here"]')
       .get('form input[type="text"]')
       .get('form input[name="title"]');
   });
-//404 error handling
+  //404 error handling
   it("Should display error message for 404 status code", () => {
     cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
       statusCode: 404,
@@ -24,13 +23,26 @@ describe("Load", () => {
       "Oops! Something went wrong!"
     );
   });
-//500 error handling
+  //500 error handling
+  it("Should display error message for 500 status code", () => {
+    cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+      statusCode: 500,
+    });
+    cy.visit("http://localhost:3000/").contains(
+      "h1",
+      "Oops! Something went wrong!"
+    );
+  });
+  //user can click to open single movie card
 
-//user can click to open single movie card
+  //search button
+  it("Should have a button that reads Search", () => {
+    cy.get(".search-button")
+      .contains("SEARCH")
+    cy.contains("X").should("not.exist")
+  });
 
-//search button
+  //test for instantiaton?
 
-//test for instantiaton?
-
-
+  //search bar functionality (CYOA?)
 });
--- cypress/integration/singleMoviePage.js
+++ cypress/integration/singleMoviePage.js
@@ -1,7 +1,36 @@
+// describe('User Movie Info Page Flows', () => {
+//   beforeEach(() => {
+//     cy.singleMovieInfo();
+//   });
+
+// true testing
+// describe("Single movie page flow", () => {
+//     beforeEach(() => {
+//       cy.singleMovieInfo();
+//     });
+describe('Single Movie view flow', () => {
+   it('Should be able to display movie poster on load', () => {
+    cy.visit('http://localhost:3000/718444')
+    cy.get('img').should('be.visible')
+   })
+
+  it("Should confirm that true is equal to true", () => {
+    expect(true).to.equal(true);
+  });
+
 //x-button
+it("It should display a button to return user to home page", () => {
+  cy.get(".x-button")
+    .click()
+    .url().should("eq", "http://localhost:3000/")
+    // .url().should("not eq", "http://localhost:3000/718444")
+})
 
 //test for missing data
 
 // header?
-
+it("Should have a header with text Rancid Tomatillos on load", () => {
+  cy.visit("http://localhost:3000/").contains("Rancid Tomatillos");
+});
 //trailer?
+});
