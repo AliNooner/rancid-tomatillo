18af436ef9d7fac50251162cffea111fabe38a10
Ali Nooner <alinooner@gmail.com>
2022-03-31 18:51:51 -0500

    Add testing for single movie page

--- cypress/integration/singleMoviePage.js
+++ cypress/integration/singleMoviePage.js
@@ -1,32 +1,37 @@
-// describe('User Movie Info Page Flows', () => {
-//   beforeEach(() => {
-//     cy.singleMovieInfo();
-//   });
-
-// true testing
-// describe("Single movie page flow", () => {
-//     beforeEach(() => {
-//       cy.singleMovieInfo();
-//     });
 describe('Single Movie view flow', () => {
    it('Should be able to display movie poster on load', () => {
     cy.visit('http://localhost:3000/718444')
     cy.get('img').should('be.visible')
    })
 
-  it("Should confirm that true is equal to true", () => {
-    expect(true).to.equal(true);
-  });
-
 //x-button
 it("It should display a button to return user to home page", () => {
   cy.get(".x-button")
     .click()
     .url().should("eq", "http://localhost:3000/")
-    // .url().should("not eq", "http://localhost:3000/718444")
 })
 
-//test for missing data
+//404 error handling
+it("Should display error message for 404 status code", () => {
+  cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+    statusCode: 404,
+  });
+  cy.visit("http://localhost:3000/718444").contains(
+    "h1",
+    "Oops! Something went wrong!"
+  );
+});
+
+//500 error handling
+it("Should display error message for 500 status code", () => {
+  cy.intercept("https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+    statusCode: 500,
+  });
+  cy.visit("http://localhost:3000/718444").contains(
+    "h1",
+    "Oops! Something went wrong!"
+  );
+});
 
 // header?
 it("Should have a header with text Rancid Tomatillos on load", () => {
